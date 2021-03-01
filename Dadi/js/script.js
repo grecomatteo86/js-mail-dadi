d4a4036f5b434df4ef7cc29f8cdf50709864ea06
Matteo <grecomatteo86@gmail.com>
2021-03-01 17:49:01 +0100

    Dadi_genero_random_utente

---
+++ Dadi/js/script.js
@@ -0,0 +1,11 @@
+//1. genero un numero random da 1 a 6 per il giocatore
+var nRandomUtente = Math.floor(Math.random() * 6) + 1;
+console.log(nRandomUtente);
+
+
+
+
+
+
+
+//. Stabilire il vincitore, in base a chi fa il punteggio più alto.
