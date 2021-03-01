f15b38b3e7babfe2c2bb6d83fa48c70d80ce2245
Matteo <grecomatteo86@gmail.com>
2021-03-01 17:58:43 +0100

    dadi_condizioni di vincita

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -2,13 +2,17 @@
 var nRandomUtente = Math.floor(Math.random() * 6) + 1;
 console.log('il numero random per l\'utente è ' + nRandomUtente);
 
+
 //2. genero un numero random da 1 a 6 per il computer
 var nRandomComputer = Math.floor(Math.random() * 6) + 1;
 console.log('il numero random per il computer è ' +nRandomComputer);
 
 
-
-
-
-
-//. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+//3. stabilisco il vincitore, in base a chi fa il punteggio più alto
+if (nRandomUtente > nRandomComputer) {
+  alert('ha vinto l\'utente');
+} else if (nRandomUtente < nRandomComputer) {
+  alert('ha vinto il computer');
+} else {
+  alert('avete pareggiato');
+}
