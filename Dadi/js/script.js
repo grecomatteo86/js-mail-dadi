e85fd2a4634d99d8bdef2831dbdc2fa4a4e4ea15
Matteo <grecomatteo86@gmail.com>
2021-07-20 14:30:29 +0200

    Dice refactoring

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,18 +1,11 @@
-//1. genero un numero random da 1 a 6 per il giocatore
-var nRandomUtente = Math.floor(Math.random() * 6) + 1;
-console.log('il numero random per l\'utente è ' + nRandomUtente);
-
-
-//2. genero un numero random da 1 a 6 per il computer
-var nRandomComputer = Math.floor(Math.random() * 6) + 1;
-console.log('il numero random per il computer è ' +nRandomComputer);
-
-
-//3. stabilisco il vincitore, in base a chi fa il punteggio più alto
-if (nRandomUtente > nRandomComputer) {
-  alert('ha vinto l\'utente');
-} else if (nRandomUtente < nRandomComputer) {
-  alert('ha vinto il computer');
+var getRandomUserNumber = Math.floor(Math.random() * 6) + 1;
+console.log('The user\'s number is ' + getRandomUserNumber);
+var getRandomComputerNumber = Math.floor(Math.random() * 6) + 1;
+console.log('The computer\'s number is ' +getRandomComputerNumber);
+if (getRandomUserNumber > getRandomComputerNumber) {
+  alert('user wins');
+} else if (getRandomUserNumber < getRandomComputerNumber) {
+  alert('computer wins');
 } else {
-  alert('avete pareggiato');
+  alert('draw');
 }
