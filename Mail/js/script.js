8fc500958a5386d794f37e3634f7421825d55e39
Matteo <grecomatteo86@gmail.com>
2021-07-20 15:35:03 +0200

    Mail refactoring

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,26 +1,27 @@
-// esercizio Mail
 
-// 1. scrivo un Array delle mail
-var listaMail = ['alessio.vietri@boolean.com','clelia.fradella@boolean.com','samuele.madrigali@boolean.com','matteo.greco@boolean.com'];
-console.log(listaMail);
-
-//2. chiedo all’utente la sua email
-var mailUtente = prompt('scrivi la tua mail');
-var risultato = 0;
-console.log('la mail inserita dall\'utente è ' + mailUtente);
-
-//3. controlla che sia nella lista di chi può accedere
-for ( var i = 0 ; i < listaMail.length; i++ ){
-  console.log(listaMail[i]);
-  if (mailUtente == listaMail[i]){
-    console.log('ok, puoi accedere');
-    risultato = 1;
+var arrayMails = ['alessio.vietri@boolean.com','clelia.fradella@boolean.com','samuele.madrigali@boolean.com','matteo.greco@boolean.com'];
+console.log(arrayMails);
+var getUserMail = prompt('Please, write your email');
+var access = 0;
+console.log('User\'s email is ' + getUserMail);
+var userMailValidation = ValidateEmail(getUserMail);
+for ( var i = 0 ; i < arrayMails.length; i++ ){
+  if (getUserMail == arrayMails[i]){
+    console.log('user\'s email is in list');
+    access = 1;
   }
 }
-
-//4. stampa un messaggio appropriato sull’esito del controllo
-if (risultato == 1) {
-  alert('puoi accedere');
+if (access == 1) {
+  alert('You are authorized');
 } else {
-  alert('non puoi accedere');
+  alert('Access denied. Sorry, you are not authorized');
+}
+function ValidateEmail(inputText) {
+  if(inputText.includes('@') && inputText.includes('.com')) {
+    alert("Valid email address!");
+    return true;
+  } else {
+  alert("You have entered an invalid email address!");
+  return false;
+  }
 }
